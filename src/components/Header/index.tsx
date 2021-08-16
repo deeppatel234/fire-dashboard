0761ebcfafaeb04a4472fe8b800753bae9771416
Deep Patel <deeppatel234@gmail.com>
2021-08-16 19:56:36 +0530

    ad fonts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./index.scss";
 
 const Header = (): JSX.Element => {
-  return <div className="main-header">header</div>;
+  return (
+    <div className="main-header">
+      <div className="workspace-block">Onlinesales.ai</div>
+    </div>
+  );
 };
 
 export default Header;
