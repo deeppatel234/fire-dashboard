c40fab53ee52c3cd7b3c013bd22de814185408e1
Deep Patel <deeppatel234@gmail.com>
2022-01-26 22:49:10 +0530

    improve select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import ReactSelect from "react-select";
 
 import "./index.scss";
 
-const Select = (props) => {
+const Select = ({
+  onChangeValue,
+  onChange,
+  value,
+  options,
+  isMulti,
+  ...props
+}) => {
+  const selectValue = useMemo(() => {
+    if (isMulti) {
+      return options.filter((o) => value?.includes?.(o.value));
+    }
+
+    return options.find((o) => o.value === value);
+  }, [value, isMulti, options]);
+
+  const onChangeData = (val) => {
+    if (onChange) {
+      onChange(isMulti ? val.map((v) => v.value) : val.value);
+    }
+
+    if (onChangeValue) {
+      onChangeValue(isMulti ? val.map((v) => v.value) : val.value);
+    }
+  };
+
   return (
     <ReactSelect
       classNamePrefix="select"
       className="react-select-wrapper"
       menuPortalTarget={document.body}
+      onChange={onChangeData}
+      value={selectValue}
+      options={options}
+      isMulti={isMulti}
       {...props}
     />
   );
