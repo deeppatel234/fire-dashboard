b4409d54ef44e768af2fea2c304c1a52de698388
Deep Patel <deeppatel234@gmail.com>
2022-02-19 09:16:06 +0530

    add icon search

--- src/components/IconSelector/index.tsx
+++ src/components/IconSelector/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import classNames from "classnames";
 
 import Popover from "components/Popover";
+import Input from "components/Input";
 
 import { iconWithGroup } from "./icons";
 
@@ -9,6 +10,35 @@ import "./index.scss";
 
 const IconSelector = ({ selectedIcon, setSelectedIcon }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const iconWithGroupData = useMemo(() => {
+    if (search) {
+      return Object.keys(iconWithGroup).reduce((memo, key) => {
+        const groupKeys = Object.keys(iconWithGroup[key]).filter((groupKey) =>
+          groupKey.toLowerCase().includes(search.toLowerCase()),
+        );
+
+        groupKeys.forEach((groupKey) => {
+          if (!memo[key]) {
+            memo[key] = {};
+          }
+
+          memo[key][groupKey] = iconWithGroup[key][groupKey];
+        });
+
+        return memo;
+      }, {});
+    }
+
+    return iconWithGroup;
+  }, [search]);
+
+  useEffect(() => {
+    if (!isOpen) {
+      setSearch("");
+    }
+  }, [isOpen]);
 
   const onClickGroupSideBar = (key) => {
     document.getElementById(`icon-group-${key}`).scrollIntoView();
@@ -22,46 +52,67 @@ const IconSelector = ({ selectedIcon, setSelectedIcon }) => {
   const renderComponent = () => {
     return (
       <div className="icon-selector-popover-content">
-        <div className="group-sidebar">
-          {Object.keys(iconWithGroup).map((key) => {
-            return (
-              <div
-                key={key}
-                className="title"
-                onClick={() => onClickGroupSideBar(key)}
-              >
-                {key}
-              </div>
-            );
-          })}
+        <div className="icon-input">
+          <Input
+            placeholder="Search icon"
+            value={search}
+            onChangeValue={setSearch}
+          />
         </div>
-        <div className="group-content-wrapper">
-          {Object.keys(iconWithGroup).map((key) => {
-            return (
-              <div key={key} className="group-content" id={`icon-group-${key}`}>
-                <div className="group-title">{key}</div>
-                <div className="group-icons">
-                  {Object.keys(iconWithGroup[key]).map((iconKey) => {
-                    return iconWithGroup[key][iconKey].icon.map((type) => {
-                      const className = `ri-${iconKey}${
-                        type ? `-${type}` : ""
-                      }`;
-                      return (
-                        <div
-                          key={type}
-                          className="group-icon-block"
-                          onClick={() => onSelectIcon(className)}
-                        >
-                          <i className={classNames("icon", className)} />
-                        </div>
-                      );
-                    });
-                  })}
-                </div>
-              </div>
-            );
-          })}
-        </div>
+        {Object.keys(iconWithGroupData).length ? (
+          <div className="icon-block">
+            <div className="group-sidebar">
+              {Object.keys(iconWithGroupData).map((key) => {
+                return (
+                  <div
+                    key={key}
+                    className="title"
+                    onClick={() => onClickGroupSideBar(key)}
+                  >
+                    {key}
+                  </div>
+                );
+              })}
+            </div>
+            <div className="group-content-wrapper">
+              {Object.keys(iconWithGroupData).map((key) => {
+                return (
+                  <div
+                    key={key}
+                    className="group-content"
+                    id={`icon-group-${key}`}
+                  >
+                    <div className="group-title">{key}</div>
+                    <div className="group-icons">
+                      {Object.keys(iconWithGroupData[key]).map((iconKey) => {
+                        return iconWithGroupData[key][iconKey].icon.map(
+                          (type) => {
+                            const className = `ri-${iconKey}${
+                              type ? `-${type}` : ""
+                            }`;
+                            return (
+                              <div
+                                key={type}
+                                className="group-icon-block"
+                                onClick={() => onSelectIcon(className)}
+                              >
+                                <i className={classNames("icon", className)} />
+                              </div>
+                            );
+                          },
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <div className="icon-block empty-block">
+            <div>No icon found</div>
+          </div>
+        )}
       </div>
     );
   };
